72fc4e2fbaacc8d57a02f19de3b91e41e2ff2efe
charlesakio <charlesjavelona@gmail.com>
2015-05-01 11:50:19 -0400

    Updated function in views to look and show title in url instead of primary key

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -8,11 +8,15 @@ from blog.models import Post
 def index(request):
 	latest_posts = Post.objects.all().order_by('-created_at')
 	template = loader.get_template('blog/index.html')
-	context = Context({'latest_posts': latest_posts, })
+	context_dict = {'latest_posts': latest_posts, }
+	for post in latest_posts:
+		post.url = post.title.replace(' ', '_')
+	context = Context(context_dict)
 	return HttpResponse(template.render(context))
 
-def post(request, post_id):
-	single_post = get_object_or_404(Post, pk=post_id)
+def post(request, post_url):
+	single_post = get_object_or_404(Post, 
+									title=post_url.replace('_', ' '))
 	template = loader.get_template('blog/post.html')
 	context = Context({'single_post': single_post, })
 	return HttpResponse(template.render(context))
