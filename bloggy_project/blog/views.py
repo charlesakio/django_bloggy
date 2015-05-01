cb70049c6059082c1039647a4b26fac53ea7d3a2
charlesakio <charlesjavelona@gmail.com>
2015-04-30 22:01:17 -0400

    Added templates folder with post and index html

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.shortcuts import get_object_or_404
 
 from blog.models import Post
 
@@ -8,4 +9,10 @@ def index(request):
 	latest_posts = Post.objects.all().order_by('-created_at')
 	template = loader.get_template('blog/index.html')
 	context = Context({'latest_posts': latest_posts, })
+	return HttpResponse(template.render(context))
+
+def post(request, post_id):
+	single_post = get_object_or_404(Post, pk=post_id)
+	template = loader.get_template('blog/post.html')
+	context = Context({'single_post': single_post, })
 	return HttpResponse(template.render(context))
