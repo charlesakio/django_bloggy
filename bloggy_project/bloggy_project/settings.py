0e924e92dbadd63a14261f02cdfc263048243fde
charlesakio <charlesjavelona@gmail.com>
2015-05-02 17:09:52 -0400

    Added django-form-bootstrap as third-party apps, can be found in add_post.htnl

--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Localapps
+
+    #Third Party Apps
+    'django_forms_bootstrap',
+
+    #Local Apps
     'blog',
 )
 
